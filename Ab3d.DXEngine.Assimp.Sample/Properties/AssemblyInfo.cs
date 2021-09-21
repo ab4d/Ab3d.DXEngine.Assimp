38ead07c57bde6d775f04fd44cc7d6f961ce40d6
Andrej Benedik <abenedik@ab4d.com>
2021-09-21 09:24:54 +0200

    Updated version to 1.1.7934

--- Ab3d.DXEngine.Assimp.Sample/Properties/AssemblyInfo.cs
+++ Ab3d.DXEngine.Assimp.Sample/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.7876.1048")]
-[assembly: AssemblyFileVersion("1.0.7876.1048")]
+[assembly: AssemblyVersion("1.1.7934.1048")]
+[assembly: AssemblyFileVersion("1.1.7934.1048")]
